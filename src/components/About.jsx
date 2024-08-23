640a3e3720cfc7026c19f3b80273ab6b5ae18058
Maximova Snezhana <kleprer@gmail.com>
2024-08-23 20:27:24 +0300

    text alignment change depending on screen size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,11 @@ const About = () => {
         <div className="flex flex-col items-center align-center md:gap-[2rem] md:flex-row">
             <img src="/pixelpfp.jpg" alt="my selfie" className="m-0 w-[20rem] rounded-lg border-white border-[3px]"/>
             <div className="w-auto lg:max-w-[70%] m-auto sm:m-0 md:m-auto">
-                <h2 className=""><span className="text-[2em]  border-b-[3px] pb-[0.5rem] border-w-fit-content">Снежана Максимова</span></h2>
-                <p className="text-[1.5em] pt-[2rem]">19 лет, Москва</p>
-                <p className="mt-[1rem]"><span className="text-[1.5em] pt-[2rem] border-b-[3px] pb-[0.5rem] border-w-fit-content">МТУСИ, программная инженерия</span></p>
-                <p className="pt-[2rem] text-[1em]">амбициозный девелопер и фанат ретро. программирую, рисую, музицирую.</p>
-                <p className="pt-[1rem] text-[1em]">html, css, tailwindcss, js, react, C</p>
+                <h2 className="text-center md:text-left"><span className="text-[2em]  border-b-[3px] pb-[0.5rem] border-w-fit-content">Снежана Максимова</span></h2>
+                <p className="text-center md:text-left text-[1.5em] pt-[2rem]">19 лет, Москва</p>
+                <p className="text-center md:text-left mt-[1rem]"><span className="text-[1.5em] pt-[2rem] border-b-[3px] pb-[0.5rem] border-w-fit-content">МТУСИ, программная инженерия</span></p>
+                <p className="text-center md:text-left pt-[2rem] text-[1em]">амбициозный девелопер и фанат ретро. программирую, рисую, музицирую.</p>
+                <p className="text-center md:text-left pt-[1rem] text-[1em]">html, css, tailwindcss, js, react, C</p>
             </div>
             
         </div>
