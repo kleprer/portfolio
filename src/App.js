5b5201b2411dfb692f02b790f42e59b1ab776ca9
Maximova Snezhana <kleprer@gmail.com>
2024-08-23 20:38:29 +0300

    added buttons for chapters. removed helping borders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import About from './components/About';
+import Chapters from './components/Chapters';
 function App() {
   return (
     <div className="w-screen h-screen bg-black">
-      <div className="w-screen h-fit bg-black">
+      <div className="w-fit h-fit bg-black">
         <About />
+        <Chapters />
       </div>
     </div>
   );
