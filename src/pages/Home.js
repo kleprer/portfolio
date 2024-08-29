6543e81ac119a7d420e83b1945751201cb85f0aa
Maximova Snezhana <kleprer@gmail.com>
2024-08-30 01:33:16 +0300

    fixed adaptive design again

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 function Home() {
   return (
     <div className="w-screen h-screen bg-black">
-      <div className="w-fit h-fit bg-black">
+      <div className="w-auto h-fit bg-black">
         <About />
         <Layout />
       </div>
