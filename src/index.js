57fa5f6347e7dc3ffca71930d92db86d8b189240
Maximova Snezhana <kleprer@gmail.com>
2024-08-30 01:29:35 +0300

    added icon for language changing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contacts from './pages/Contacts';
+import { TbWorld } from "react-icons/tb";
+import Layout from './components/Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +15,9 @@ root.render(
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Unbounded:wght@200..900&display=swap" rel="stylesheet"></link>
     <BrowserRouter>
+      <div className="bg-black w-screen h-fit pt-[0.5rem] pb-[1rem] sm:pr-[0.5rem] pr-[0] relative">
+        <TbWorld className="size-[3rem] relative left-[40%] sm:left-[45%] md:left-[92%] md:fixed text-white w-fit hover:opacity-[50%] hover:cursor-pointer " />
+      </div>
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="contacts" element={<Contacts />} />
