013f97aae6e76736d9609c32486df518c1c6a924
Maximova Snezhana <kleprer@gmail.com>
2024-08-30 01:49:14 +0300

    adaptive width for layout changed again to percents

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,9 +3,9 @@ import { Link, Outlet } from 'react-router-dom';
 const Chapters = () => {
   return (
     <div className="w-auto flex flex-col sm:flex-row text-white mt-[2rem] md:mt-[5rem] m-auto gap-[1rem] bg-black">
-        <Link to='/' className="text-center w-[20rem] sm:w-[22rem] m-auto p-[10px]  border-[2px] mb-[15px] hover:opacity-[50%]"><button >Обо мне</button></Link>
-        <Link to='/projects' className="text-center w-[20rem] sm:w-[22rem] m-auto p-[10px]  border-[2px] mb-[15px] hover:opacity-[50%]"><button >Проекты</button></Link>
-        <Link to='/contacts' className="text-center w-[20rem] sm:w-[22rem] m-auto p-[10px] border-[2px] mb-[15px] hover:opacity-[50%]"><button >Контакты</button></Link>
+        <Link to='/' className="text-center w-[20rem] sm:w-[28%] m-auto p-[10px]  border-[2px] mb-[15px] hover:opacity-[50%]"><button >Обо мне</button></Link>
+        <Link to='/projects' className="text-center w-[20rem] sm:w-[28%] m-auto p-[10px]  border-[2px] mb-[15px] hover:opacity-[50%]"><button >Проекты</button></Link>
+        <Link to='/contacts' className="text-center w-[20rem] sm:w-[28%] m-auto p-[10px] border-[2px] mb-[15px] hover:opacity-[50%]"><button >Контакты</button></Link>
         <Outlet />
     </div>
   )
