f2a9b23b844212356232c3610bbe1f93fe48d312
Maximova Snezhana <kleprer@gmail.com>
2024-09-02 19:11:30 +0300

    width adaptation for project cards descriptions

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -9,7 +9,7 @@ const ProjectCard = (props) => {
     <div className="min-h-[28.5rem] w-auto xl:w-[70rem] h-fit  text-white p-[1rem] md:p-[2rem] bg-black">
         <div className="flex flex-col lg:flex-row items-center align-center gap-[2rem] md:pt-[2rem]">
             <img src={t(`projects.${pj}.source`)} alt="project" className="max-h-[24rem] max-w-[25rem] m-0"/>
-            <div className="w-auto max-w-[60%] m-auto sm:m-0 md:m-auto text-center md:text-left ">
+            <div className="w-auto md:max-w-[60%] m-auto sm:m-0 md:m-auto text-center md:text-left ">
                 <h2><span className="text-[2em]  border-b-[3px] pb-[0.5rem] border-w-fit-content">{t(`projects.${pj}.title`)}</span></h2>
                 <p className="mt-[1rem]">{t(`projects.${pj}.desc`)}</p>
                 <p className="text-[1em] pt-[2rem]">{t(`projects.${pj}.longDesc`)}</p>
