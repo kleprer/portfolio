4270f69512a4318849edac6402c8608df2f1c78f
Maximova Snezhana <kleprer@gmail.com>
2024-09-02 19:05:55 +0300

    fixed projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,9 +5,11 @@ import projects from '../translations/ru/global.json'
 const Projects = () => {
   const [projectId, changeProjectId] = useState(1);
   console.log(projectId);
-  const fakeProjects = [projects.projects];
+  const fakeProjects = [projects.projects['1'], projects.projects['2'], projects.projects['3'],];
+  console.log(fakeProjects);
+  useEffect(() => {
 
-  
+  }, [projectId])
 
   const swipeProject = (side, projectId) => {
     const id = projectId;
@@ -25,15 +27,16 @@ const Projects = () => {
         changeProjectId(id + 1);
       }
     }
+    console.log(projectId);
   };
 
   return (
     <div className="w-auto bg-black text-white min-h-[32rem] pb-[1rem]">
       <div className="bg-black text-white h-fit">
-        <div className="flex flex-row gap-[1rem]">
-          <button onClick={() => swipeProject(1, projectId)}><span className="text-[3em] hover:opacity-[50%]">&lt;</span></button>
+        <div className="flex flex-row gap-[1rem] items-center align-center">
+          <button onClick={() => swipeProject(1, projectId)}><span className=" text-[3em] hover:opacity-[50%]">&lt;</span></button>
           <ProjectCard project={projectId}/>
-          <button onClick={() => swipeProject(2, projectId)}><span className="text-[3em] hover:opacity-[50%]">&gt;</span></button>
+          <button onClick={() => swipeProject(2, projectId)}><span className="max-h-fit text-[3em] hover:opacity-[50%]">&gt;</span></button>
         </div>
         {/* <Layout /> */}
         
